4f8c1f4e1bffc024affc834f02b577fd1ee669f0
mitalig21-maker <mitalig21@gmail.com>
2026-04-07 20:52:43 -0400

    Initial Rally build

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE = 'rally-v1';
+const FILES = [
+  '/rally-app/',
+  '/rally-app/index.html',
+  '/rally-app/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
